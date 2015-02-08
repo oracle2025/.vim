a897ef4158f1e17a9234824de3c622b31208b2ba
Richard Spindler <richard.spindler@gmail.com>
2015-02-08 13:09:32 +0100

    emoji font for linux

--- install-script.sh
+++ install-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install build-essential vim-gtk git cmake python-dev cppreference-doc-en-html
+sudo apt-get install ttf-ancient-fonts #For Emojis?
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
 
