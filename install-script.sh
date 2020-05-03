32f776374ecd9f8ccf76cb6ca98041e7d5f81049
Richard Spindler <richard.spindler@gmail.com>
2020-05-03 17:31:02 +0200

    rm

--- install-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get install build-essential vim-gtk git cmake python-dev cppreference-doc-en-html
-sudo apt-get install ttf-ancient-fonts #For Emojis?
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
-#For Chrome: https://chrome.google.com/webstore/detail/chromoji-hangouts-edition/fgaiicnhcohdgegabomljlkheilficfd?hl=en-GB
-
-
-
-
